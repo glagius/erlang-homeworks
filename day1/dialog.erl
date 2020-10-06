6a2a7121ac429b76c5719cfd6598f944120baccb
Eldar Babaev <el.glagius@gmail.com>
2020-10-06 11:38:56 +0700

    Fixed script. Added .gitignore file

--- day1/dialog.erl
+++ day1/dialog.erl
@@ -1,20 +1,18 @@
 -module(dialog).
 
--export([dialog/0]).
+-export([start/0]).
 
-dialog() ->
-    io:format('Hello man! What is your name?~n'),
+start() ->
+    io:format("Hello man! What is your name?~n"),
     {_, [Name]} = io:fread("My name is: ", "~s"),
     {_, [Age]} = io:fread("How old are you, ", "~s"),
     Description =
 	io:get_line("Write something about yourself: "),
-    {_, File} = file:open("About,txt", [write]),
-    io:fwrite(File, "~s",
-	      string:join(["~nMy name is: ", Name], " ")),
-    io:fwrite(File, "~s",
-	      string:join(["~nMy age is: ", Age], " ")),
-    io:fwrite(File, "~s",
-	      string:join(["~nSome words about myself: ",
-			   Description],
-			  " ")),
+    {_, File} = file:open("About.txt", [write]),
+    io:fwrite(File, "~s", ["\nMy name is: "]),
+    io:fwrite(File, "~s", [Name]),
+    io:fwrite(File, "~s", ["\nMy age is: "]),
+    io:fwrite(File, "~s", [Age]),
+    io:fwrite(File, "~s", ["\nSome words about myself: "]),
+    io:fwrite(File, "~s", [Description]),
     halt().
